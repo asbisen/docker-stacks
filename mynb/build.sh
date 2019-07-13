39bac50da7ab39cd5dee291dd98100b63ebc5aea
Anand Bisen <asbisen@icloud.com>
2019-07-13 21:28:52 +0000

    add vaex - mynb

---
+++ mynb/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SRV="srv.apnet.local:5000"
+NAME="mynb"
+TAG="latest"
+DT=`date +%y-%m-%d`
+
+docker build . -t ${SRV}/${NAME}:${TAG}
+docker tag ${SRV}/${NAME}:${TAG} ${SRV}/${NAME}:${DT}
+docker push ${SRV}/${NAME}:${TAG}
+docker push ${SRV}/${NAME}:${DT}
